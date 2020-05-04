c763f0b05b9ac2e6b71b29e3cfa14d09fac71a48
Asbjørn Djupdal <djupdal@ntnu.no>
2020-05-04 12:05:50 +0200

    Small updates to windeploy script.

--- windeploy.sh
+++ windeploy.sh
@@ -3,16 +3,12 @@
 windeployqt bin/lynsyn_viewer.exe
 windeployqt bin/lynsyn_xvc.exe
 cp /c/msys64/mingw64/bin/libbz2-1.dll bin/
-cp /c/msys64/mingw64/bin/libdouble-conversion.dll bin/
 cp /c/msys64/mingw64/bin/libfreetype-6.dll bin/
 cp /c/msys64/mingw64/bin/libgcc_s_seh-1.dll bin/
 cp /c/msys64/mingw64/bin/libglib-2.0-0.dll bin/
 cp /c/msys64/mingw64/bin/libgraphite2.dll bin/
 cp /c/msys64/mingw64/bin/libharfbuzz-0.dll bin/
 cp /c/msys64/mingw64/bin/libiconv-2.dll bin/
-cp /c/msys64/mingw64/bin/libicudt65.dll bin/
-cp /c/msys64/mingw64/bin/libicuin65.dll bin/
-cp /c/msys64/mingw64/bin/libicuuc65.dll bin/
 cp /c/msys64/mingw64/bin/libintl-8.dll bin/
 cp /c/msys64/mingw64/bin/libpcre-1.dll bin/
 cp /c/msys64/mingw64/bin/libpcre2-16-0.dll bin/
@@ -22,6 +18,9 @@ cp /c/msys64/mingw64/bin/libusb-1.0.dll bin/
 cp /c/msys64/mingw64/bin/libwinpthread-1.dll bin/
 cp /c/msys64/mingw64/bin/libzstd.dll bin/
 cp /c/msys64/mingw64/bin/zlib1.dll bin/
+cp /c/msys64/mingw64/bin/libicuin64.dll bin/
+cp /c/msys64/mingw64/bin/libicuuc64.dll bin/
+cp /c/msys64/mingw64/bin/libicudt64.dll bin/
 cp /mingw64/bin/addr2line bin/
 cp /mingw64/bin/nm bin/
 cp jtagdevices bin/jtagdevices
